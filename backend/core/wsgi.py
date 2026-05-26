8c473915500e4e8bdb01511eccfdeb57831c2ced
Sohit Giri <girisohit000@gmail.com>
2026-05-26 20:22:27 +0545

    Fix nested backend folder routing schema for vercel deployment

--- backend/core/wsgi.py
+++ backend/core/wsgi.py
@@ -1,5 +1,9 @@
 import os
 from django.core.wsgi import get_wsgi_application
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
 application = get_wsgi_application()
+
+# This is critical for Vercel's python builder to hook into Django
 app = application
